e05c32822047ae1758e8cfdd99b565c211ac778e
Antonios Alexopoulos <xviia42@gmail.com>
2023-09-04 23:27:46 +0300

    add:some colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,15 @@ import styles from "@/styles/home.module.css";
 import MainSubTitle from "@/lib/MainSubTitle";
 import Categories from "@/lib/Categories";
 
+import * as React from "react";
+
 export default function Home() {
 
-  //TODO make the categories tab stick left
+  const [addExpense,setAddExpense] = React.useState<boolean>(false);
+
+  function handleAddExpense(e: React.MouseEventHandler<SVGSVGElement>) {
+    setAddExpense(addExpense ? false : true);
+  }
 
   return (
       <div>
@@ -25,12 +31,12 @@ export default function Home() {
                 <div className={styles.infoContainer}>
                   <div className={styles.infoContainerItemMain}>
                     <MainSubTitle />
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={handleAddExpense}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
 
                   </div>
-                  <div><NewExpense /></div>
+                  {addExpense && <div><NewExpense /></div>}
                 </div>
                 <div className={styles.cardContainer}>
                   <div className={styles.cardContainerItem}><ExpensesCard title={"Harry Potter"} cost={12} date={"01/02/2022"} comments={"Have to buy this book for a gift."}/></div>
